4d396f6b009d20e5152210757211becdbb17192f
teddyking <ed@teddyking.co.uk>
2018-07-16 06:57:03 +0100

    Challenge 1: Longest word

---
+++ longestword/src/lib.rs
@@ -0,0 +1,59 @@
+pub fn longest_word(sen: &str) -> String {
+    let words = sen.split_whitespace();
+    let mut longest = String::new();
+
+    for word in words {
+        let stripped_word = strip_non_alpha_numeric(word);
+        if stripped_word.to_string().len() > longest.len() {
+            longest = stripped_word.to_string();
+        }
+    }
+
+    return longest;
+}
+
+fn strip_non_alpha_numeric(sen: &str) -> String {
+    let mut stripped_string = String::new();
+
+    for c in sen.chars() {
+        if c.is_alphabetic() {
+            stripped_string.push(c)
+        }
+    }
+
+    stripped_string
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod longest_word {
+        use super::*;
+
+        #[test]
+        fn with_a_simple_sentence() {
+            let sen = "The cake is a lie";
+
+            assert_eq!(longest_word(sen), "cake");
+        }
+
+        #[test]
+        fn with_a_word_with_punctuation() {
+            let sen = "The cake is a lie!!";
+
+            assert_eq!(longest_word(sen), "cake");
+        }
+    }
+
+    mod strip_non_alpha_numeric {
+        use super::*;
+
+        #[test]
+        fn strips_non_alpha_numeric_chars() {
+            let sen = "LOL!";
+
+            assert_eq!(strip_non_alpha_numeric(sen), "LOL");
+        }
+    }
+}
---
+++ longestword/src/main.rs
@@ -0,0 +1,5 @@
+extern crate longestword;
+
+fn main() {
+    longestword::longest_word("");
+}
